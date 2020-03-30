57e248eccc5cb31ddf84bebaa63c704a4b790231
Alex-Azh <59451772+Alex-Azh@users.noreply.github.com>
2020-03-30 10:13:25 +0500

    30/03 support database from txt

--- data_provider.cs
+++ data_provider.cs
@@ -138,6 +138,7 @@ namespace Telephone_Ring
                     string lv_sql = @"insert into t_Abonents values (@AID, @inn, @phone, @address)";
                     using (var lo_cmd = new NpgsqlCommand(lv_sql, lo_conn))
                     {
+                        //fiewkogew - добавить get_id для t_Abonents
                         lo_cmd.Parameters.AddWithValue("@AID", "A"+r.Next(1000).ToString());
                         lo_cmd.Parameters.AddWithValue("@inn", inn.Pop().ToString());
                         lo_cmd.Parameters.AddWithValue("@phone", phone.Pop().ToString());
